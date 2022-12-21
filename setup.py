f541c02c4ec888bef384e33f1992ce19da176fd0
Seppe De Langhe <seppedelanghe@gmail.com>
2022-12-21 20:13:50 +0100

    update name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sportplots",
+    name="sportsplots",
     version="0.1.0",
     author="seppedelanghe",
     author_email="seppedelanghe17@gmail.com",
